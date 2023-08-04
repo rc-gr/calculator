58e3c16809f077d6a2d41644387f3340468703f6
RCGR <138417636+rc-gr@users.noreply.github.com>
2023-08-04 17:50:59 +0800

    Add operate to choose basic operation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let first = 0;
+let second = 0;
+let operation = '';
+
 function add(a, b) {
   return +a + +b;
 }
@@ -12,4 +16,20 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(op, a, b) {
+  switch(op) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      alert('Invalid operation!');
+      break;
+  }
 }
